d54e2627213db11e313026f6441ceebafbdfc8e5
luchikkkk <108338027+luchikkkk@users.noreply.github.com>
2023-05-31 18:13:31 +0300

    Add files via upload

---
+++ EX6a/_str.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstring>
+#include <string.h>
+using namespace std;
+class Str;		//Предварительная декларация
+class _str {
+	char* m_pszData;
+	int m_nCount;
+	friend class Str;	//Дружественный класс или метод
+	_str() {
+		m_pszData = new char[1] {0}; m_nCount = 1;
+	}
+	_str(const char* p) {
+		m_nCount = 1;
+		m_pszData = new char[strlen(p) + 1];
+		strcpy_s(m_pszData, strlen(p) + 1, p);
+	}
+	~_str() { delete[]m_pszData; }
+	void AddRef() { m_nCount++; } 	//Удаляет себя v
+	void Release() { m_nCount--; if (m_nCount == 0) delete this; }
+};
+
---
+++ EX6a/list.cpp
@@ -0,0 +1,13 @@
+﻿#include "Str.h"
+#include <iostream>
+
+int main() {
+    Str s("Hello world!");
+    int index = s.find("world", 5);
+    if (index != -1) {
+        std::cout << "Substring found at index " << index << std::endl;
+    }
+    else {
+        std::cout << "Substring not found" << std::endl;
+    }
+}
